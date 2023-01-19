fcc60bab521a036c29adabdee6571adf0703c949
Samuel Ortega <samy9093@gmail.com>
2023-01-19 00:40:19 -0600

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Sammy on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
