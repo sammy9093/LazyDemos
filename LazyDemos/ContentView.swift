8da4191f5e259a61d9d01d737cbf9953904e6dc7
Samuel Ortega <samy9093@gmail.com>
2023-01-19 01:16:25 -0600

    LazyH Lazy stackable demonstration

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columns = [
+//        GridItem(.fixed(100), spacing: 1),
+        GridItem(.flexible(maximum: 45)),
+        GridItem()
+    
+    ]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: columns) {
+                ForEach(0..<100) { i in
+                    let _ = print("Dang \(i)")
+                    RedAndCyanView()
+                }
+            }
         }
         .padding()
     }
 }
 
+
+
+//Struct made for a View
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack {
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
